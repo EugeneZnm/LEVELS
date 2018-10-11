12ca7952daa988ff932603fdb4bece892887297e
EugeneZnm <eugenenzioki@gmail.com>
2018-10-11 22:39:23 +0300

    add profile

--- tuzwa/models.py
+++ tuzwa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+
+from django.dispatch import receiver
+
+
 # Create your models here.
+class Profile(models.Model):
+    """
+    method to create profile
+    """
+    avatar = models.ImageField(upload_to='media/', null=True)
+    Bio = models.CharField(max_length=2000)
+    # deletion of profile and user when deleted
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    location = models.CharField(max_length=200)
+    phone_number = models.IntegerField(default=0)
+    email = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user.username
