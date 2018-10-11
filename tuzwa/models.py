448a635770eefc7a3095d1dc020cf16e3a87b85e
EugeneZnm <eugenenzioki@gmail.com>
2018-10-11 23:04:58 +0300

    class models for scoring criteria

--- tuzwa/models.py
+++ tuzwa/models.py
@@ -45,7 +45,7 @@ class Profile(models.Model):
 # adding the project profile
 class Project(models.Model):
     """
-    Image model creating table
+    Project model creating table
     """
     image = models.ImageField(upload_to='media', blank=True)
     image2 = models.ImageField(upload_to='media', blank=True)
@@ -54,3 +54,63 @@ class Project(models.Model):
     caption = models.CharField(max_length=1000)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User , on_delete=models.CASCADE, default=True)
+
+    def save_project(self):
+        """
+        method to save project images
+        :return:
+        """
+        self.save()
+
+    @classmethod
+    def get_project(cls, project_id):
+        """
+        method to get image by id
+        :return:
+        """
+        projects = cls.objects.filter(id=project_id)
+        return projects
+
+    def delete_image(self):
+        """
+        method to delete image
+        :return:
+        """
+        self.delete()
+
+
+# models for voting criterion
+class Design(models.Model):
+    """
+    model for design measure criterion
+    """
+    design_score = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='design', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+class Usability(models.Model):
+    """
+    model for usability
+    """
+    usability_score = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='design', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+
+class Creativity(models.Model):
+    """
+    model for scoring creativity
+    """
+    creativity_score = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='creativity', null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+
+
+class Content(models.Model):
+    """
+    model for scoring content
+    """
+    content_score =models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project', null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
