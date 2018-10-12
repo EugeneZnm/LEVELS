b0b09f4c70aa4f08b56029eb84866179d5b79c99
EugeneZnm <eugenenzioki@gmail.com>
2018-10-12 12:12:29 +0300

    database configuration

--- tuzo/settings.py
+++ tuzo/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config,Csv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tuzwa',
+    'bootstrap4',
+    'crispy_forms',
+    'widget_tweaks',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -76,8 +84,10 @@ WSGI_APPLICATION = 'tuzo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tuzwa',
+        'USER': 'eugene',
+        'PASSWORD': 'necromancer'
     }
 }
 
@@ -119,3 +129,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# setting logout redirect
+LOGOUT_REDIRECT_URL = 'home'
+
+# login redirect
+LOGIN_REDIRECT_URL = 'profile'
+
+# defining the login url of application
+LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
